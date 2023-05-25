1aa6acf19ad051830b1d6413f55d040843400e5d
Mohamed AlMahroos <MAlmahroos@slb.com>
2023-05-25 10:45:04 +0400

    Few changes in title

--- streamlit_ui/Home.py
+++ streamlit_ui/Home.py
@@ -5,7 +5,7 @@ import PIL
 import matplotlib.pyplot as plt
 
 
-st.title("Hello!!")
+st.title("Welcome to our city sustainability dashboard!!")
 data_file = st.file_uploader(label='Upload an Image')
 # st.balloons()
 image = PIL.Image.open(data_file)
