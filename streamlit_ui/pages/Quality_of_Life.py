9e8d59ee8845996a0024db12fe24e7d4dd66b139
Mohamed AlMahroos <MAlmahroos@slb.com>
2023-05-25 11:23:47 +0400

    removed emojis

--- streamlit_ui/pages/Quality_of_Life.py
+++ streamlit_ui/pages/Quality_of_Life.py
@@ -5,7 +5,7 @@ import PIL
 import matplotlib.pyplot as plt
 
 # Title
-st.title("Watch our model do some magic! Upload an image and get the quality of life prediction \U+1F609")
+st.title("Watch our model do some magic! Upload an image and get the quality of life prediction")
 
 # Add some text
 st.write("Our quality of life preduction first divides the classes into 3 metrics:")
@@ -16,6 +16,6 @@ st.write("    Sum of the percentages of Developed Space, Road, and Building")
 st.write("3. Land_metric")
 st.write("    Sum of the percentages of Bareland, Agriculture land, and Other")
 st.write("************")
-st.write("Later on these metrics are classified into High, Medium and Low quality of life \U0001F929\U0001F929\U0001F929")
+st.write("Later on these metrics are classified into High, Medium and Low quality of life")
 
 data_file = st.file_uploader(label='Upload an Image')
