62191213f0731ff9db4e832d9511e21a0f8b6565
Mohamed AlMahroos <MAlmahroos@slb.com>
2023-05-25 11:15:18 +0400

    Added some text

---
+++ streamlit_ui/pages/Quality_of_Life.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import PIL
+import matplotlib.pyplot as plt
+
+# Title
+st.title("Watch our model do some magic! Upload an image and get the quality of life prediction")
+
+# Add some text
+st.write("Our quality of life preduction first divides the classes into 3 metrics:")
+st.write("1. Environmental_metric")
+st.write("    Sum of the percentages of Rangeland, Tree, and Water")
+st.write("2. Infrastructure_metric")
+st.write("    Sum of the percentages of Developed Space, Road, and Building")
+st.write("3. Land_metric")
+st.write("    Sum of the percentages of Bareland, Agriculture land, and Other")
+st.write("************")
+st.write("Later on these metrics are classified into High, Medium and Low quality of life")
+
+data_file = st.file_uploader(label='Upload an Image')
