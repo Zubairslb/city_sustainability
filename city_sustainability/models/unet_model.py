3677f98d03b00ad9775148956921f14225468ef7
Zubair <zzubair@slb.com>
2023-05-26 15:52:23 +0500

    package for unit model

--- city_sustainability/models/unet_model.py
+++ city_sustainability/models/unet_model.py
@@ -83,9 +83,8 @@ def train_model(model, x, y, epochs=1, batch_size=32, validation_split=0.1, clas
     class_weights_dict = None
     
     if class_balance:
-        class_labels = np.unique(np.argmax(y, axis=1))
-        y_flat = np.argmax(y, axis=1)
-        class_weights = class_weight.compute_class_weight('balanced', class_labels, y_flat)
+        class_labels = np.unique(y)
+        class_weights = class_weight.compute_class_weight('balanced', class_labels, y)
         class_weights_dict = dict(zip(class_labels, class_weights))
     
     model.compile(optimizer=Adam(learning_rate=0.0001), loss='categorical_crossentropy', metrics=['accuracy', compute_iou])
