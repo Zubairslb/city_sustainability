37fe921dde7b559d4ff38b7252640eeedc6dd15b
Mallikarjuna Rao <mgudipati@slb.com>
2023-05-29 12:58:49 +0530

    Intro

--- streamlit_ui/Home.py
+++ streamlit_ui/Home.py
@@ -5,9 +5,9 @@ import PIL
 import matplotlib.pyplot as plt
 
 
-st.title("Hello!!")
-data_file = st.file_uploader(label='Upload an Image')
-# st.balloons()
-image = PIL.Image.open(data_file)
-st.image(image, caption='Sunrise by the mountains')
-
+st.title(f"City Sustainability: ​Utilizing CNN Deep Learning to Compute Quality of Life Metrics​")
+# data_file = st.file_uploader(label='Upload an Image')
+# # st.balloons()
+# image = PIL.Image.open(data_file)
+# st.image(image, caption='Sunrise by the mountains')
+st.image('intro_image.jpg')
