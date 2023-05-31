f0155bccb613dad6b475e4dd82e8376bab6994c8
Zubair <zzubair@slb.com>
2023-05-31 15:06:45 +0500

    streamlit update for image path

--- streamlit_ui/Home.py
+++ streamlit_ui/Home.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import PIL
 import matplotlib.pyplot as plt
+import os
 
 
 st.title("Welcome to our City Sustainability Platform​ :smile:")
@@ -11,7 +12,8 @@ st.write("## What is City Sustainability? :city_sunrise:")
 st.write("#### City sustainability refers to the concept of creating and maintaining cities that are environmentally, socially, and economically sustainable. It involves developing urban areas that minimize their impact on the environment, promote social equity, and support economic prosperity.")
 
 # Provide the path or URL of the image
-image = "Images/City.jpg"
+path = os.path.dirname(__file__)
+image = path+"/Images/City.jpg"
 # or image_url = "https://example.com/image.jpg"
 
 # Display the image
@@ -30,7 +32,7 @@ st.write("- The encoder is the first half in the architecture diagram. It usuall
 st.write(" - The decoder is the second half of the architecture. The goal is to semantically project the discriminative features (lower resolution) learnt by the encoder onto the pixel space (higher resolution) to get a dense classification. The decoder consists of upsampling and concatenation followed by regular convolution operations.")
 
 # Provide the path or URL of the image
-image_path = "Images/UNet Model.PNG"
+image_path = path+"/Images/UNet Model.PNG"
 # or image_url = "https://example.com/image.jpg"
 
 # Display the image
@@ -42,7 +44,7 @@ st.write("## Dataset :books:")
 st.write("#### The Dataset used to train our model consists of certain classes and annotations as shown below:")
 
 # Provide the path or URL of the image
-image_path_2 = "Images/Dataset Classes.png"
+image_path_2 = path+"/Images/Dataset Classes.png"
 # or image_url = "https://example.com/image.jpg"
 
 # Display the image
@@ -58,7 +60,7 @@ st.write("## About Us :raised_hand:")
 st.write("#### We are a group of SLB engineers coming from various backgrounds and united in our interest in Data Science :computer:")
 
 # Provide the path or URL of the image
-image_path_3 = "Images/Logos.PNG"
+image_path_3 = path+"/Images/Logos.PNG"
 # or image_url = "https://example.com/image.jpg"
 
 # Display the image
